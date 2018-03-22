a0bf21f5fa83a90cd057809a6f41f3745700573f
OptIn <bbtarzan12@naver.com>
2018-03-23 00:49:32 +0900

    TileBase 기반 CustomTile 테스트 작성

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Player : MonoBehaviour
+{
+    
+    Tilemap currentTilemap;
+    
+    void Awake()
+    {
+        currentTilemap = GameObject.Find("Grid").transform.Find("Ground").GetComponent<Tilemap>();
+    }
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector3 hitPosition = Vector3.zero;
+        if (currentTilemap != null && currentTilemap.gameObject == col.gameObject)
+        {
+            foreach (ContactPoint2D hit in col.contacts)
+            {
+                hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
+                hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
+                Vector3Int cellPosition = currentTilemap.WorldToCell(hitPosition);
+                CustomTile customTile = currentTilemap.GetTile<CustomTile>(cellPosition);
+                if(customTile != null)
+                    customTile.Action();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tiles/CustomTile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+public class CustomTile : TileBase
+{
+    [SerializeField] Sprite tileSprite;
+
+    public void Action()
+    {
+        Debug.Log("Actoin");
+    }
+    
+    #if UNITY_EDITOR
+    [MenuItem("Assets/Create/CustomTile")]
+    public static void CreateCustomTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Custom Tile", "New Custom Tile", "Asset", "Save Custom Tile", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(CreateInstance<CustomTile>(), path);
+    }
+    #endif
+    
+}
