04520e90b571368e8abd13a1ef077b82337decc5
OptIn <bbtarzan12@naver.com>
2018-04-05 10:24:07 +0900

    Abnormal CSV 데이터 파싱 추가

---
+++ Assets/Scripts/Core/Structs/AbnormalStruct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+[Serializable]
+public struct AbnormalStruct
+{
+    public int cid;
+    public string name;
+    public string nameKor;
+    public string abnormalType;
+    public string target;
+
+    public AbnormalStruct(int cid, string name, string nameKor, string abnormalType, string target)
+    {
+        this.cid = cid;
+        this.name = name;
+        this.nameKor = nameKor;
+        this.abnormalType = abnormalType;
+        this.target = target;
+    }
+
+    public override string ToString()
+    {
+        return string.Format("{0} | {1} | {2} | {3} | {4}", cid, name, nameKor, abnormalType, target);
+    }
+}
---
+++ Assets/Scripts/Data/AbnormalData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AbnormalData : ScriptableObject
+{
+    public List<AbnormalStruct> Data { get; private set; }
+
+    [MenuItem("Assets/Data/Abnormal")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectExtension.CreateAsset<AbnormalData>();
+    }
+
+    public List<AbnormalStruct> Load()
+    {
+        Data = new List<AbnormalStruct>();
+        List<Dictionary<string, object>> csv = CSVParser.Read(Resources.Load<TextAsset>("Data/CSV/abnormal"));
+        foreach (Dictionary<string, object> line in csv)
+        {
+            AbnormalStruct abnormalStruct = new AbnormalStruct(
+                int.Parse(line["cid"].ToString()),
+                line["name"].ToString(),
+                line["name_kor"].ToString(),
+                line["abnormal_Type"].ToString(),
+                line["target"].ToString()
+            );
+            Data.Add(abnormalStruct);
+        }
+
+        return Data;
+    }
+}
