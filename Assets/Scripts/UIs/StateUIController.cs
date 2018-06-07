bcd508474d94f6dd3792021b3204dbfb17aaf035
OptIn <bbtarzan12@naver.com>
2018-06-07 17:52:47 +0900

    StateUI Controller 완성 및 Core 폴더 리펙토링

---
+++ Assets/Scripts/UIs/StateUIController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StateUIController : MonoBehaviour
+{
+    [SerializeField] [ReadOnlyAttribute(true)] [Range(0, 1)] float hpPercent;
+    [SerializeField] [ReadOnlyAttribute(true)] [Range(0, 1)] float gaugePercent;
+    
+    public float HpPercent { get { return hpPercent; }}
+    public float GaugePercent { get { return gaugePercent; }}
+
+    [SerializeField] Slider hpSlider;
+    [SerializeField] Transform gaugeTransform;
+    Quaternion gaugeRotation;
+
+    [SerializeField] float gaugeSpeed = 3;
+    [SerializeField] float gaugeRandomness = 10;
+
+    public void UpdateUI()
+    {
+        hpSlider.value = HpPercent;
+        gaugeRotation = gaugeTransform.localRotation;
+        Vector3 gaugeEuler = gaugeRotation.eulerAngles;
+        gaugeEuler.z  = 270 * gaugePercent - 45 + Random.Range(-gaugeRandomness, gaugeRandomness);
+        gaugeRotation.eulerAngles = gaugeEuler;
+        gaugeTransform.rotation = Quaternion.Slerp(gaugeTransform.rotation, gaugeRotation, Time.deltaTime * gaugeSpeed);
+    }
+
+    void Update()
+    {
+        UpdateUI();
+    }
+}
