f4e8c7e31c089531b2d0cec4b5a541177cb9bed0
OptIn <bbtarzan12@gmail.com>
2018-07-24 23:54:43 +0900

    새로운 AI를 위한 Behaviour Tree 추가

---
+++ Assets/Scripts/Core/AI/Nodes/InverterNode.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class InverterNode : IParentBehaviourTreeNode
+{
+    string name;
+    
+    IBehaviourTreeNode childNode;
+
+    public InverterNode(string name)
+    {
+        this.name = name;
+    }
+
+    public BehaviourTreeStatus Tick(TimeData time)
+    {
+        if (childNode == null)
+        {
+            throw new ApplicationException("InverterNode must have a child node!");
+        }
+
+        var result = childNode.Tick(time);
+        if (result == BehaviourTreeStatus.Failure)
+        {
+            return BehaviourTreeStatus.Success;
+        }
+
+        if (result == BehaviourTreeStatus.Success)
+        {
+            return BehaviourTreeStatus.Failure;
+        }
+
+        return result;
+    }
+
+
+    public void AddChild(IBehaviourTreeNode child)
+    {
+        if (childNode != null)
+        {
+            throw new ApplicationException("Can't add more than a single child to InverterNode!");
+        }
+
+        childNode = child;
+    }
+}
